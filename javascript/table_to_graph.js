7c0fa2ac2501e5a48e2334a57d071848eebb2465
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-01-23 11:23:33 -0500

    done, this one was hard so come back to it

--- javascript/table_to_graph.js
+++ javascript/table_to_graph.js
@@ -1,5 +1,24 @@
 function tableToGraph(friends) {
   // type your code here
+  const result = {};
+  let newfriends = friends.replace(
+    /<tr>|<\/tr>|<table>|<\/table>|<th>|<\/th>/gm,
+    ""
+  );
+  const friendsarr = newfriends.split(/<td>|<\/td>/gm).slice(1, -1);
+  for (let i = 0; i < friendsarr.length; i += 4) {
+    let current = friendsarr[i];
+    let adjacent =
+      friendsarr[i + 2] === "" ? [] : friendsarr[i + 2].split(", ");
+
+    result[current] = adjacent;
+
+    for (let friend of adjacent) {
+      if (!result[friend]) result[friend] = [];
+      if (!result[friend].includes(current)) result[friend].push(current);
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
@@ -10,23 +29,44 @@ if (require.main === module) {
   }
 
   // add your own tests in here
-  const friends = "<table><tr><th>Person</th><th>Friends</th></tr><tr><td>Fred</td><td>Jane, Carol, Anesh, Xi</td></tr><tr><td>Carol</td><td>Fred, Anesh, Janelle</td></tr></table>";
-  const result = {
+  let friends =
+    "<table><tr><th>Person</th><th>Friends</th></tr><tr><td>Fred</td><td>Jane, Carol, Anesh, Xi</td></tr><tr><td>Carol</td><td>Fred, Anesh, Janelle</td></tr></table>";
+  let result = {
     Fred: ["Jane", "Carol", "Anesh", "Xi"],
     Jane: ["Fred"],
     Carol: ["Fred", "Anesh", "Janelle"],
     Anesh: ["Fred", "Carol"],
     Xi: ["Fred"],
-    Janelle: ["Carol"]
+    Janelle: ["Carol"],
   };
 
+  //const newfriends = friends.replace(/<[a-z]*>|<\/[a-z]*>/gm, " ");
+  //let newfriends = friends.replace(/<td>|<\/td>/gm, " ");
+  // let newfriends = friends.replace(
+  //   /<tr>|<\/tr>|<table>|<\/table>|<th>|<\/th>/gm,
+  //   ""
+  // );
+
+  // console.log(newfriends.split(/<td>|<\/td>/gm).slice(1, -1));
+
   console.log("Expecting: ");
-  console.log(printResults(result));
+  printResults(result);
   console.log("");
   console.log("Got: ");
-  console.log(printResults(tableToGraph(friends)));
+  printResults(tableToGraph(friends));
 
   console.log("");
+
+  friends =
+    "<table><tr><th>Person</th><th>Friends</th></tr><tr><td>Gremlin</td><td></td></tr></table>";
+  result = {
+    Gremlin: [],
+  };
+  console.log("Expecting: ");
+  printResults(result);
+  console.log("");
+  console.log("Got: ");
+  printResults(tableToGraph(friends));
 }
 
 module.exports = tableToGraph;
